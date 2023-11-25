5a35f855c120f23f59b96f524a4631316b0bfa9c
FilFib <fibekb4@gmail.com>
2023-11-25 17:50:30 +0100

    accounts/change/registration

--- NBAjesrseyShop/accounts/views.py
+++ NBAjesrseyShop/accounts/views.py
@@ -1,13 +1,15 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import RegistrationForm, AddressForm
+from django.shortcuts import redirect
 
 
 class RegistrationView(CreateView):
     template_name = 'registration/registration.html'
     form_class = RegistrationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('registration')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -15,7 +17,9 @@ class RegistrationView(CreateView):
         return context 
     
     def form_valid(self, form):
-        user = form.save()
+        user = form.save(commit=False)
+        user.save()
+
         address_form = AddressForm(self.request.POST)
         if address_form.is_valid():
             address = address_form.save(commit=False)
@@ -24,5 +28,18 @@ class RegistrationView(CreateView):
         else:
             user.delete()
             return self.form_invalid(form)
+
         login(self.request, user)
-        return super().form_valid(form)
+        return redirect(self.success_url)
+
+
+class CustomLogoutView(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        return redirect(reverse_lazy('registration'))
+
+
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return redirect(reverse_lazy('registration'))
