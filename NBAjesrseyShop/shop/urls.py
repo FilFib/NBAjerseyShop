44d4df410e254c85846c8ee1eb5fcc3a97e13a49
Filip Fibakiewicz <fibekb4@gmail.com>
2023-12-16 13:37:53 +0100

    shop and cart / change / displaying the number of available products message when trying to add more products to the cart than are in stock

--- NBAjesrseyShop/shop/urls.py
+++ NBAjesrseyShop/shop/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', HomeViews.as_view(), name='home'),
     path('team_products/<pk>', TeamProductsListViews.as_view(), name='team_products'),
     path('product_details/<pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('product_details/<pk>/<is_out_of_stock>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product_details/<pk>/<is_out_of_stock>/<product_variant_id>', ProductDetailView.as_view(), name='product_detail'),
 ]
 
 if settings.DEBUG:
