8a4e60e9f981182e242a4663961a1dc8c5ce547e
Filip Fibakiewicz <fibekb4@gmail.com>
2023-12-22 15:05:15 +0100

    removing null=True from models if they have blank=True

--- NBAjesrseyShop/orders/models.py
+++ NBAjesrseyShop/orders/models.py
@@ -5,10 +5,10 @@ from shop.models import ProductVariant
 
 class Order(models.Model):
     order_date = models.DateTimeField(auto_now_add=True)
-    payment_date = models.DateTimeField(blank=True, null=True)
+    payment_date = models.DateTimeField(blank=True)
     status = models.CharField(max_length=20,default="placed")
     total_cost = models.DecimalField(max_digits=8, decimal_places=2)
-    address_id = models.ForeignKey(Address, on_delete=models.DO_NOTHING, null=True)
+    address_id = models.ForeignKey(Address, on_delete=models.DO_NOTHING, blank=True)
 
     def __str__(self):
         return f'Order id: {self.id} | {self.address_id.user_id} | Order date: {self.order_date} | Total cost: {self.total_cost} | Status: {self.status}'
